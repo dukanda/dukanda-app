508e96d6036032051b8969e8c86b9600b27782cf
dukanda <dukanda.ao@gmail.com>
2025-03-23 00:29:45 +0000

    trying deploy

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -17,7 +17,6 @@ type ToasterToast = ToastProps & {
   description?: React.ReactNode
   action?: ToastActionElement
 }
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
@@ -32,7 +31,6 @@ function genId() {
   count = (count + 1) % Number.MAX_SAFE_INTEGER
   return count.toString()
 }
-
 type ActionType = typeof actionTypes
 
 type Action =
